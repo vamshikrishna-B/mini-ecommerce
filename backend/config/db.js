b622ca8940a7cd6806165d0d62fabfaeb95cf2eb
vamshi krishna -B <bvamshikrishna1912@gmail.com>
2025-11-03 12:54:25 +0530

    connected db

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+const connectDb = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", error);
+    }
+}
+
+export default connectDb;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDb from '../config/db.js';
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
+  connectDb()
   console.log(`Server is running on port ${port}`);
 });
